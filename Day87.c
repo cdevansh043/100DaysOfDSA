5239a48b745e2233f8c1819027b9debfd2cdb5c9
cdevansh043 <cdevansh043@gmail.com>
2026-05-01 10:03:50 +0530

    Day-87 Done

---
+++ Day87.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+int main() {
+    int n;
+    if (scanf("%d", &n) != 1) return 0;
+
+    int *arr = (int *)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    qsort(arr, n, sizeof(int), compare);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d%s", arr[i], (i == n - 1) ? "" : " ");
+    }
+    printf("\n");
+
+    free(arr);
+    return 0;
+}
